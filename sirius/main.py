50c10faa3d92b75a87cc6563f633c4a0a61acb45
jvogel <joevogel501@gmail.com>
2025-04-09 17:48:05 -0700

    implemented the CLI call of sirius, updated tests

--- sirius/main.py
+++ sirius/main.py
@@ -2,12 +2,10 @@ import uvicorn
 import tempfile
 import subprocess
 import os
-import csv
 from fastapi import FastAPI
 from starlette.middleware.cors import CORSMiddleware
 from pydantic import BaseModel
 from typing import List
-
 
 app = FastAPI()
 
@@ -15,7 +13,7 @@ class PCM(BaseModel):
     pre_cursor_mass: float
 
 class Peak(BaseModel):
-    mz: float # m/z mass to charge ratio
+    mz: float # m/z: mass to charge ratio
     intensity: int # intensity
 
 class MSMS(BaseModel):
@@ -30,7 +28,9 @@ app.add_middleware(
     allow_headers = ["*"],
 )
 
-# takes in an msms value represented as a string and parses it into MSMS data type
+
+
+# takes in an msms represented as a string and parses it into MSMS object
 def parse_msms_string(msms_str: str) -> MSMS:
     temp_peaks: List[Peak] = []
     for pair in msms_str.strip().split():
@@ -39,23 +39,30 @@ def parse_msms_string(msms_str: str) -> MSMS:
         temp_peaks.append(peak)
     return MSMS(peaks=temp_peaks)
 
-# takes in MSMS and PCM and creates a temporary MGF file to be passed to the sirius CLI command as input
+
+
+# takes in MSMS and PCM and creates a temporary MGF file to be passed to the sirius CLI command 
 # returns the file path of that MGF file as a string
 def create_mgf_file(msms: MSMS, pcm: PCM) -> str:
     mgf_content = ["BEGIN IONS"]
     mgf_content.append(f"PEPMASS={pcm.pre_cursor_mass}")
     for peak in msms.peaks:
-        mgf_content.append(f"{peak.mz}, {peak.intensity}")
+        mgf_content.append(f"{peak.mz} {peak.intensity}")
     mgf_content.append("END IONS")
     
     with tempfile.NamedTemporaryFile(mode="w", suffix=".mgf", delete=False) as tmpMGF:
         tmpMGF.write("\n".join(mgf_content))
         return tmpMGF.name # returns the path of the temporary MGF file as a string
 
-# takes in the MGF file as argument, as well as output and summary directory, returns a formula_candidates.tsv file in summary dir
-# probably hardcode the output and summary directories on the database:
-# def run_sirius_CLI(mgf_file_path: str) -> str:
-def run_sirius_CLI(mgf_file_path: str, output_dir: str, summary_dir: str) -> str:
+
+
+# takes in the MGF file as argument, returns formula_candidates.tsv file path
+def run_sirius_CLI(mgf_file_path: str) -> str:
+# if we want to change output directories, change function signature to:
+# def run_sirius_CLI(mgf_file_path: str, output_dir: str, summary_dir: str) -> str:
+    output_dir = "sirius-output"
+    summary_dir = "sirius-summary"
+
     command = [
         "sirius",
         "--input", mgf_file_path,
@@ -66,18 +73,35 @@ def run_sirius_CLI(mgf_file_path: str, output_dir: str, summary_dir: str) -> str
         "structure",
         "compound-classes",
         "write-summaries",
-        "--output", summary_dir
+        "--output", summary_dir 
     ]
-
     subprocess.run(command, check=True)
 
     os.remove(mgf_file_path) # delete temporary MGF file as it is no longer needed after being passed to sirius command
-    return f"{summary_dir}/...TODO" # TODO, how do we get the filepath of the formula_candidates.tsv file
 
-# takes in the formula_candidates.tsv file and parses them into a list of strings
-def parse_sirius_output(formula_candidates_file: str) -> list[str]:
-    #TODO 
-    pass
+    # grab the base name of the mgf file, remove .mgf extension
+    base = os.path.splitext(os.path.basename(mgf_file_path))[0] 
+
+    # return the path to the formula_candidates.tsv file
+    # half-hardcoded path, this might need to be changed, TODO test this feature 
+    tsv_path = os.path.join(summary_dir, f"0_{base}_FEATURE1", "formula_candidates.tsv")
+    return tsv_path
+
+
+
+# takes in the formula_candidates.tsv file and parses the compounds into a list of strings
+def parse_sirius_output(formula_candidates_tsv_path: str) -> list[str]:
+    candidates_list = []
+
+    with open(formula_candidates_tsv_path, "r") as file:
+        lines = file.readlines()
+        for line in lines[1:11]: # skip header line
+            values = line.strip().split("\t") # strip \n and split by tabs, list of values per line
+            candidates_list.append(values[1]) # values at index 1 hold molecular formula
+
+    return candidates_list
+
+
 
 # a post request taking in MSMS and PCM, returning a list of the 10 most probable elements in order of probability
 # responce as a JSON
@@ -91,12 +115,13 @@ def get_compounds(msms_str: str, pcm_str: str) -> list[str]:
     topten = parse_sirius_output(candidates)
     return topten
 
+    '''
     dummy_compounds = [
         "H2O", "CO2", "H2", "O2", "H2SO4",
         "NaCl", "CH4", "C2H6", "C3H8", "KCl"
     ]
-    # implement use of sirius here to get the elements
     return dummy_compounds
+    '''
 
 if __name__ == "__main__":
     uvicorn.run("main:app", port=80, log_level="info")
--- tests/test_sirius.py
+++ tests/test_sirius.py
@@ -4,23 +4,18 @@ from sirius.main import app
 client = TestClient(app)
 
 def test_post_get_compounds():
-    # sample payload for post request
-    # TODO: fix the format of the payload with new MSMS fields
-    payload = {
-        "msms": {
-            "ion1": 1,
-            "ion2": 2,
-            "ion3": 3
-        },
-        "pcm": {
-            "pre_cursor_mass": 50
-        }
-    }
+    # Arbitrary test data:
+    msms_str = "55.963573:5938.000000 56.798489:6062.000000 64.927414:10277.000000 65.293808:5628.000000 67.389580:5226.000000 70.065033:18834.000000 73.024750:5421.000000 78.059624:5925.000000 84.080620:1603720.000000 88.075508:12758.000000 89.620560:5591.000000 110.096336:16728.000000 123.964256:10125.000000 128.106689:107149.000000 129.114563:180595.000000 146.059540:22846.000000 146.071335:25567.000000 146.117233:1189030.000000 147.053940:7681.000000"
+    pcm_str = "23.6145"
 
-    response = client.post("/", json=payload)
+    # post request
+    response = client.post("/", params={"msms_str": msms_str, "pcm_str": pcm_str})
 
+    # HTTP response code should be 200 OK
     assert response.status_code == 200
+
     data = response.json()
     assert isinstance(data, list)
     assert len(data) == 10
     assert all(isinstance(item, str) for item in data)
+ 
