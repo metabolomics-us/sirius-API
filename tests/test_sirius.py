a3cf289663ca1f1da80bd2a46ba0f98a4b885371
jvogel <joevogel501@gmail.com>
2025-04-14 16:58:58 -0700

    updated tests with new error codes and messages

--- tests/test_sirius.py
+++ tests/test_sirius.py
@@ -3,18 +3,43 @@ from sirius.main import app
 
 client = TestClient(app)
 
-def test_post_get_compounds():
+def test_valid_post_200():
     # Arbitrary test data:
     msms_str = "189.48956:1.9 283.62076:3.4 301.22977:66.3 311.08008:1.3 399.99106:2.3"
     pcm_str = "413.26611887841"
 
     # post request
-    response = client.post("/", params={"msms_str": msms_str, "pcm_str": pcm_str})
+    response = client.post("/compounds", params={"msms_str": msms_str, "pcm_str": pcm_str})
 
-    # HTTP response code should be 200 OK
+    # HTTP response code should be 200 - OK
     assert response.status_code == 200
 
     data = response.json()
     assert isinstance(data, list)
     assert all(isinstance(item, str) for item in data)
+
+
  
+def test_invalid_PCM_input():
+    msms_str = "189.48956:1.9 283.62076:3.4 301.22977:66.3 311.08008:1.3 399.99106:2.3"
+    pcm_str = "fail"
+
+    # post request
+    response = client.post("/compounds", params={"msms_str": msms_str, "pcm_str": pcm_str})
+
+    # HTTP response code should be 400 - client error
+    assert response.status_code == 400
+    assert response.json()["detail"] == "Invalid precursor mass format. Expected a number."
+
+
+
+def test_invalid_MSMS_input():
+    msms_str = "should:fail"
+    pcm_str = "413.26611887841"
+
+    # post request
+    response = client.post("/compounds", params={"msms_str": msms_str, "pcm_str": pcm_str})
+
+    # HTTP response code should be 400 - client error
+    assert response.status_code == 400
+    assert response.json()["detail"] == "Invalid MSMS format. Expected 'mz:intensity' number pairs."
